d79dfcce5b27510803c5597ee5f863525cf8b096
Vortilad <mattlee3262@gmail.com>
2018-03-22 18:31:51 -0500

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -14,23 +14,14 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(()=> {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    //console.log(visibleExpenses);
-    console.log(state);
-});
+//store.subscribe(()=> {
+//    const state = store.getState();
+//    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//    //console.log(visibleExpenses);
+//    console.log(state);
+//});
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent bill', amount: 1230000, createdAt: 1000}));
-const expenseTwo = store.dispatch(addExpense({description: 'Groceries', amount: 50000000, createdAt: -1000}));
-const expenseTree = store.dispatch(addExpense({description: 'Utilities bill', amount: 133200, createdAt: -300}));
-const expenseOe = store.dispatch(addExpense({description: 'Netflix bill', amount: 100000, createdAt: -22400}));
-const expenseTo = store.dispatch(addExpense({description: 'Hulu bill', amount: 5032000, createdAt: 1520442000000 }));
-const expenseThre = store.dispatch(addExpense({description: 'Gym', amount: 84300, createdAt: 3300}));
-const expenseTw = store.dispatch(addExpense({description: 'Cable bill', amount: 42000000, createdAt: 4000}));
 //store.dispatch(setTextFilter('bill'));
-
-
 //setTimeout(() => {
     //store.dispatch(setTextFilter('hulu'));
 //}, 3000) 
