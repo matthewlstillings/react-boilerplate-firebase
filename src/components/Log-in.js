2c1183c57b8541ec634b9e07cff602fba8065a30
matthewlstillings <mstillingswebdesign@gmail.com>
2018-06-27 00:18:29 -0500

    Login page style

--- src/components/Log-in.js
+++ src/components/Log-in.js
@@ -4,10 +4,15 @@ import {startLogin} from '../actions/auth';
 
 
 export const LogIn = ({startLogin}) => (
-            <div>
-                <button onClick={startLogin}>Log In</button>
-                <a>Forgot your info?</a>
+        <div className="log-in">
+            <div className="log-in__container">
+                <div className="log-in__text__container">  
+                    <h1 className="log-in__text is--title">Empty Wallet</h1>
+                    <p className="log-in__text is--subtitle">What's in your wal-  ...nevermind.</p>
+                </div>
+                <button onClick={startLogin} className="log-in__btn btn">Log In</button>
             </div>
+        </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
